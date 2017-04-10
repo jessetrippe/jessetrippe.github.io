0430ae7c68f53bc6af9b89e083d1e718999d2be5
Jesse Trippe <jesse@jessetrippe.com>
2017-04-09 18:47:18 -0700

    Adding to main option

--- sprout-social/js/main.js
+++ sprout-social/js/main.js
@@ -1,7 +1,13 @@
 $( document ).ready(function() {
 	$('.js-complete').click(function() {
+		var currentMessage = $(this).closest('.js-message');
+
 		$(this).addClass('dismiss_complete');
-		$(this).closest('.js-message').addClass('collapse');
-		$(this).closest('.js-message').addClass('collapse-collapsed');
+		currentMessage.addClass('collapse');
+		currentMessage.addClass('collapse-collapsed');
+	    currentMessage.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',   
+	    function(e) {
+	    	$(this).remove();
+		});
 	});
 });
