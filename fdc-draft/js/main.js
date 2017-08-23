8d3f51f53e644b2e15a6941ab5d2d9d40f23b8de
Jesse Trippe <jesse@jessetrippe.com>
2017-08-23 00:22:02 -0700

    Slowing down scrolling

--- fdc-draft/js/main.js
+++ fdc-draft/js/main.js
@@ -2,5 +2,5 @@ $( ".remove-player" ).click(function() {
   $(this).closest("tr").toggleClass("removed");
   $('html, body').animate({
     scrollTop: $("tbody tr:not(.removed):first").offset().top
-  }, 200);
+  }, 300);
 });
