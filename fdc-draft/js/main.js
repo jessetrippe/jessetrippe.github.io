c6b0d8e4a8adf18087224421df81f505e73188f0
Jesse Trippe <jesse@jessetrippe.com>
2017-10-05 21:27:44 -0700

    Removing FDC Project and adding TFC

--- fdc-draft/js/main.js
+++
@@ -1,6 +0,0 @@
-$( ".remove-player" ).click(function() {
-  $(this).closest("tr").toggleClass("removed");
-  $('html, body').animate({
-    scrollTop: $("tbody tr:not(.removed):first").offset().top
-  }, 300);
-});
