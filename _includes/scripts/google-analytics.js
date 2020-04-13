1cca6c53550c000643d63939e42b6210b82a3d9b
Jesse Trippe <jesse@jessetrippe.com>
2020-04-13 16:17:28 -0700

    Removing old includes

--- _includes/scripts/google-analytics.js
+++
@@ -1,1 +0,0 @@
-!function(e,t,a,n,c,o){e.GoogleAnalyticsObject=n,e[n]||(e[n]=function(){(e[n].q=e[n].q||[]).push(arguments)}),e[n].l=+new Date,c=t.createElement(a),o=t.getElementsByTagName(a)[0],c.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script","ga"),ga("create","UA-54788171-1","auto"),ga("send","pageview");
